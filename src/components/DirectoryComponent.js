3e2738ec34fbeba2568c52b18fb6d7124ed6319f
Daniel Wykes <daniel.j.wykes@alumni.psu.edu>
2021-08-23 23:05:55 -0400

    Components Part 2

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,56 +1,64 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Directory extends Component {
         
         constructor(props){ // whenever class component created with constructor, must include props
             super(props); // always first line of constructor, required
             this.state = {
-                campsites: [
-                     {
-                         id: 0,
-                         name: 'React Lake Campground',
-                         image: 'assets/images/react-lake.jpg',
-                         elevation: 1233,
-                         description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
-                     }, {
-                         id: 1,
-                         name: 'Chrome River Campground ',
-                         image: 'assets/images/chrome-river.jpg',
-                         elevation: 877,
-                         description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
-                     }, {
-                         id: 2,
-                         name: 'Breadcrumb Trail Campground',
-                         image: 'assets/images/breadcrumb-trail.jpg',
-                         elevation: 2901,
-                         description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
-                     }, {
-                         id: 3,
-                         name: 'Redux Woods Campground',
-                         image: 'assets/images/redux-woods.jpg',
-                         elevation: 42,
-                         description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
-                     }
-                ]
+                selectdCampsite: null
             };
+        }
+
+        onCampsiteSelect(campsite) {
+            //Set selected campsite to clicked campsite
+            //Never set state directly utilizing this.state.selectedCampsite = campsite; ALWAYS use setState
+            this.setState({selectdCampsite: campsite});
+        }
+
+        renderSelectedCampsite(campsite) {
+            //Campsite has object in it before trying to build card
+            if(campsite) {
+                return (
+                    <Card>
+                        <CardImg top src={campsite.image} alt={campsite.name} />
+                        <CardBody>
+                            <CardTitle>{campsite.name}</CardTitle>
+                            <CardText>{campsite.description}</CardText>
+                        </CardBody>
+                    </Card>
+                );
+            }
+            return <div />;
         }
 
         render() {
 
-            const directory = this.state.campsites.map(campsite => {
+            // Loop through campsites and return card for each one 
+            const directory = this.props.campsites.map(campsite => {
                 return (
-                    <div key={campsite.id} className="col">
-                        <img src={campsite.image} alt={campsite.name} />
-                        <h2>{campsite.name}</h2>
-                        <p>{campsite.description}</p>
+                    <div key={campsite.id} className="col-md-5 m-1">
+                        <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                            <CardImg width = "100%" src={campsite.image} alt={campsite.name} />
+                            <CardImgOverlay>
+                                <CardTitle>{campsite.name}</CardTitle>
+                            </CardImgOverlay>
+                        </Card>
                     </div>
                 );
             });
 
             return (
+
+                // Create container containing all campsites in directory
                 <div className="container">
                     <div className="row">
                         {directory}
+                    </div>
+                    <div className="row">
+                        <div className="col-md-5 m-1">
+                            {this.renderSelectedCampsite(this.state.selectdCampsite)}
+                        </div>
                     </div>
                 </div>
             );
