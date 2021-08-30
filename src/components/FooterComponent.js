583efcaab93600f2943d1aecb806ac554436be50
Daniel Wykes <daniel.j.wykes@alumni.psu.edu>
2021-08-30 16:51:52 -0400

    Week 3 Workshop: Add about us page and format

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer(props) {
     return (
@@ -8,18 +9,19 @@ function Footer(props) {
                     <div className="col-4 col-sm-2 offset-1">
                         <h5>Links</h5>
                         <ul className="list-unstyled">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Directory</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Contact</a></li>
+                            <li><Link to="/home">Home</Link></li>
+                            <li><Link to="/directory">Directory</Link></li>
+                            <li><Link to="/aboutus">About</Link></li>
+                            <li><Link to="/contactus">Contact</Link></li>
                         </ul>
                     </div>
                     <div className="col-6 col-sm-3 text-center">
                         <h5>Social</h5>
-                        <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram" /></a>{' '}
-                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-facebook" /></a>{' '}
-                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter" /></a>{' '}
-                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube" /></a> 
+                        {/* {' '} is white space in JSX */}
+                        <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/" target="_blank" rel="noreferrer"><i className="fa fa-instagram" /></a>{' '}
+                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/" target="_blank" rel="noreferrer"><i className="fa fa-facebook" /></a>{' '}
+                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/" target="_blank" rel="noreferrer"><i className="fa fa-twitter" /></a>{' '}
+                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/" target="_blank" rel="noreferrer"><i className="fa fa-youtube" /></a> 
                     </div>
                     <div className="col-sm-4 text-center">
                         <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone" /> 1-206-555-1234</a><br />
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,34 +3,63 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments'; 
+import { PARTNERS } from '../shared/partners'; 
+import { PROMOTIONS } from '../shared/promotions'; 
+
 
 class Main extends Component {
     constructor(props){
         super(props);
         this.state = {
             campsites: CAMPSITES,
-            selectedCampsite: null
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
-
-    onCampsiteSelect(campsiteId) {
-        //Set selected campsite to clicked campsite
-        //Never set state directly utilizing this.state.selectedCampsite = campsite; ALWAYS use setState
-        this.setState({selectedCampsite: campsiteId});
-    }
-
-
     render() {
+
+        const HomePage = () => {
+            return (
+                <Home 
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
+            );
+        }
+
+        const CampsiteWithId = ({match}) => {
+            return (
+                // Convert match.params.campsiteId to number with +
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} 
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}    
+                />
+            );
+        };
+
         return ( 
             <div>
 
                 <Header />
-                {/* Send on click method with props */}
-                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
-                {/* Send campsite by ID from index 0 in array created by filter, CampsiteInfo expecting an object */}
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Switch>
+                    <Route path='/home' component={HomePage}></Route>
+                    {/* Send on click method with props, need to use anonymous function 
+                        to send props to component we want to route to*/}
+                    <Route exact path='/directory/' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
+                    <Redirect to='/home' />
+                </Switch>
                 <Footer />
 
             </div>
