7b2030561e0243a18c249b15eb96aa8bbc175f6d
Daniel Wykes <daniel.j.wykes@alumni.psu.edu>
2021-09-14 12:19:34 -0400

    Modify contact component Week 4

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 
     render() {
         return ( 
-            <Provider store>
+            <Provider store={store}>
                 <BrowserRouter>
                     <div className="App">
                         <Main />
