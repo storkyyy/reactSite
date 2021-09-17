519f64feb738c76589d0c35560c754a29f7117e9
Daniel Wykes <daniel.j.wykes@alumni.psu.edu>
2021-09-16 20:56:32 -0400

    Week 5: Adding redux-thunk and redux-logger

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Modal, Label, ModalHeader, ModalBody } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 const minLength = len => val => val && (val.length >= 2);
 const maxLength = len => val => !val || (val.length <= 15);
@@ -47,6 +48,28 @@ function RenderCampsite({ campsite }) {
 
 
 function CampsiteInfo(props) {
+
+    if(props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+
+    if(props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     // Ensure campsite passed to component
     if (props.campsite) {
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes';
+import { CAMPSITES } from '../shared/campsites';
 
 export const addComment = (campsiteId, rating, author, text) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -9,4 +10,36 @@ export const addComment = (campsiteId, rating, author, text) => ({
         author,
         text
     }
-})
+});
+
+export const fetchCampsites = () => dispatch => {
+
+    // Pass store's dispatch method and dispatch this action
+    dispatch(campsitesLoading());
+
+    // Brief delay before dispatching addCampsites action
+    setTimeout(() => {
+        dispatch(addCampsites(CAMPSITES));
+    }, 2000);
+
+};
+
+export const campsitesLoading = () => ({
+
+    type: ActionTypes.CAMPSITES_LOADING
+    
+});
+
+export const campsitesFailed = errMess => ({
+
+    type: ActionTypes.CAMPSITES_FAILED,
+    payload: errMess
+
+});
+
+export const addCampsites = campsites => ({
+
+    type: ActionTypes.ADD_CAMPSITES,
+    payload: campsites
+
+});
